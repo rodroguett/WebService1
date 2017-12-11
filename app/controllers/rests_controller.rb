3d89180b047b697d4f48357bc9aaf47c45102a0b
rodroguett <rodroguett@gmail.com>
2017-12-11 15:54:09 -0300

    adding suport to hhtps

--- app/controllers/rests_controller.rb
+++ app/controllers/rests_controller.rb
@@ -4,8 +4,16 @@ class RestsController < ApplicationController
   def login # enviar los datos al ws2
     image_data = Base64.encode64(File.open(params[:user]["image"].tempfile, "rb").read)
     params[:user]["image"] = image_data
-    result = Net::HTTP.post_form(URI.parse('https://tranquil-fjord-82118.herokuapp.com/rest/verify_user'), params[:user])
-    redirect_to action: "result", message: result.message 
+
+    uri = URI.parse('https://tranquil-fjord-82118.herokuapp.com/rest/verify_user')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data(params[:user])
+    response = http.request(request)
+
+    redirect_to action: "result", message: response.message 
   end
 
   def requester
